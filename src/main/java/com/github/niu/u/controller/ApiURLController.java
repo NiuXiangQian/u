e794b105b126c7d33ab996fdf81ebd387f352a2b
laoniu <1603565290m>
2020-12-24 15:14:59 +0800

    update

--- src/main/java/com/github/niu/u/controller/ApiURLController.java
+++ src/main/java/com/github/niu/u/controller/ApiURLController.java
@@ -6,7 +6,6 @@ import com.github.niu.u.model.ApiGenerateReq;
 import com.github.niu.u.service.ApiAccessService;
 import com.github.niu.u.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
