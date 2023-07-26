20366cffd53729ba196886e3b9a85bc1f215943d
niuxiangqian <niuxiangqian677@gmail.com>
2023-07-26 22:55:04 +0800

    bugfix

--- src/main/java/com/github/niu/u/common/util/MD5Util.java
+++ src/main/java/com/github/niu/u/common/util/MD5Util.java
@@ -1,6 +1,5 @@
 package com.github.niu.u.common.util;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
@@ -13,7 +12,7 @@ import java.nio.charset.StandardCharsets;
  **/
 public class MD5Util {
     public static String encryptStr(String str){
-        return  DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8)).substring(8,17)+ RandomStringUtils.random(3);
+        return  DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8)).substring(8,17);
 
     }
 }
