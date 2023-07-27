dba5adf64d55e634621d7ff88d5fe3debf8a5eb6
niuxiangqian <niuxiangqian677@gmail.com>
2023-07-27 17:21:08 +0800

    bugfix

--- src/main/java/com/github/niu/u/controller/URLController.java
+++ src/main/java/com/github/niu/u/controller/URLController.java
@@ -1,10 +1,11 @@
 package com.github.niu.u.controller;
 
 import com.github.niu.u.common.core.R;
-import com.github.niu.u.common.exception.BaseException;
-import com.github.niu.u.model.vo.ShortUrlVo;
+import com.github.niu.u.config.MvcExceptionHandler;
 import com.github.niu.u.service.URLService;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,15 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("url")
 public class URLController {
+    private final static Logger logger = LoggerFactory.getLogger(URLController.class);
+
 
     @Resource
     private URLService urlService;
 
     @RequestMapping("/generate")
-    public R<Object> generate(String url, Long validTime){
+    public R<Object> generate(String url, Long validTime) {
+        logger.info("url:{},validTime:{}", url, validTime);
         if (StringUtils.isNotBlank(url)) {
             return R.failed("url不能为空");
         }
