ea618e0385fa1a802e68e51f2f88fae5de40d215
niuxiangqian <niuxiangqian677@gmail.com>
2023-07-27 17:27:52 +0800

    bugfix

--- src/main/resources/static/static/js/index.js
+++ src/main/resources/static/static/js/index.js
@@ -1,34 +1,34 @@
-layui.config().use(['form','jquery','layer'], function(){
+layui.config().use(['form', 'jquery', 'layer'], function () {
 
-   var $ = layui.$;
-   var form = layui.form;
+    var $ = layui.$;
+    var form = layui.form;
 
 
-    form.on("submit(generate)",function(){
-        var  url = $('#url_textarea').val()
-        if (url.length<1) {
+    form.on("submit(generate)", function () {
+        var url = $('#url_textarea').val()
+        if (url.length < 1) {
             layer.alert("请输入url")
-        }else {
-            index  =null
+        } else {
+            index = null
             $.ajax({//异步请求返回给后台
-                url:'/url/generate',
-                type:'POST',
+                url: '/url/generate',
+                type: 'POST',
                 data: {
-                    "url":url
+                    "url": url
                 },
-                dataType:'json',
-                beforeSend: function(re){
-                    index = top.layer.msg('生成中',{icon: 16,time:false});
+                dataType: 'json',
+                beforeSend: function (re) {
+                    index = top.layer.msg('生成中', {icon: 16, time: false});
                 },
-                success:function(d){
-                    var r=d.result;
+                success: function (d) {
+                    let r = d.result;
                     console.log(r);
-                    if(d.success){
+                    if (d.success) {
                         top.layer.msg("生成成功！");
-                        $('#result_a').text(d.data.shortUrl).attr("href",d.data.shortUrl)
-                        $('#result_div').css("display","block")
-                    }else {
-                        top.layer.msg("生成失败！");
+                        $('#result_a').text(d.data.shortUrl).attr("href", d.data.shortUrl)
+                        $('#result_div').css("display", "block")
+                    } else {
+                        top.layer.msg(`生成失败！${d.msg}`);
                     }
                     layer.close(index)
 
